36864412098ec972aa68a88db26f953847f0cb63
Rick Halterman <halterman@southern.edu>
2019-01-07 17:25:13 -0500

    Version: Mon Jan  7 17:25:12 STD 2019

---
+++ 124_W19/X-letter/x.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	std::cout << "XXX       XXX\n";
+	std::cout << "  X       X\n";
+	std::cout << "   X     X\n";
+	std::cout << "    X   X\n";
+	std::cout << "     X X\n";
+	std::cout << "      X\n";
+	std::cout << "     X X\n";
+	std::cout << "    X   X\n";
+	std::cout << "   X     X\n";
+	std::cout << "  X       X\n";
+	std::cout << "XXX       XXX\n";
+}
