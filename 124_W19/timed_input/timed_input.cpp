134054c1bedf7d492a05d4a3031754178b2bc0f9
Rick Halterman <halterman@southern.edu>
2019-02-18 15:23:28 -0500

    Version: Mon Feb 18 15:22:41 STD 2019

---
+++ 124_W19/timed_input/timed_input.cpp
@@ -0,0 +1,19 @@
+// timed_input.cpp
+// Created 2/18/2019 2:51:30 PM
+
+#include <iostream>
+#include <ctime>
+
+int main() {
+	clock_t start_time, end_time;
+	int input;
+	std::cout << "Please enter an integer: ";
+	std::cin >> input;
+	std::cout << "Please enter an integer: ";
+	start_time = clock();
+	std::cin >> input;
+	end_time = clock();
+	std::cout << static_cast<double>(end_time - start_time)/CLOCKS_PER_SEC << '\n';
+
+    
+}
